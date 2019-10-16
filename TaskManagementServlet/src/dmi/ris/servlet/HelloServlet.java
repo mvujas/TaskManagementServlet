b12591140f89f4608f7e7f83a1c6e349239c1b93
Danijela Boberic Krsticev <dboberic@uns.ac.rs>
2019-10-16 13:19:53 +0200

    sitne izmene

--- TaskManagementServlet/src/dmi/ris/servlet/HelloServlet.java
+++ TaskManagementServlet/src/dmi/ris/servlet/HelloServlet.java
@@ -29,8 +29,8 @@ public class HelloServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	/*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Hello "+request.getParameter("name"));
-	}*/
+	}
 
 }
