8e90a589e3f50e76fe9dff53089582b2843de9bb
Danijela Boberic Krsticev <dboberic@uns.ac.rs>
2019-10-23 00:01:49 +0200

    primer upotrebe skripleta

--- TaskManagementServlet/src/dmi/ris/servlet/HelloServlet.java
+++ TaskManagementServlet/src/dmi/ris/servlet/HelloServlet.java
@@ -26,12 +26,14 @@ public class HelloServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		out.append("<html>");
 		out.append("<body>");
 		out.append("<h3>Ovo je primer stranice koju je generisao Servlet </h3>");
 		out.append("Hello ");
 		out.append(request.getParameter("username"));
 		out.append("<br>");
 		out.append("</body>");
+		out.append("</html>");
 	}
 
 	/**
