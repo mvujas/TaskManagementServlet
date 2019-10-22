00a9612e4e012747b09b5f6de2e12d1a5761937e
Danijela Boberic Krsticev <dboberic@uns.ac.rs>
2019-10-22 19:15:57 +0200

    dodato da helloServlet generise html stranicu

--- TaskManagementServlet/src/dmi/ris/servlet/HelloServlet.java
+++ TaskManagementServlet/src/dmi/ris/servlet/HelloServlet.java
@@ -1,6 +1,8 @@
 package dmi.ris.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,7 +25,13 @@ public class HelloServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Hello world");
+		PrintWriter out = response.getWriter();
+		out.append("<body>");
+		out.append("<h3>Ovo je primer stranice koju je generisao Servlet </h3>");
+		out.append("Hello ");
+		out.append(request.getParameter("username"));
+		out.append("<br>");
+		out.append("</body>");
 	}
 
 	/**
